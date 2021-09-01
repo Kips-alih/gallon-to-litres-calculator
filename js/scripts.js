0bbf2801989a15acced2f99c0ee2e838142ca8d2
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-01 18:10:35 +0300

    Use arrow functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-var volumeInLitres=function(gallons,theConstant){
-  return gallons*theConstant;
-};
+var volumeInLitres=(gallons,theConstant)=>gallons*theConstant;
 var gallons=parseInt(prompt("Enter the water volume in gallons:"));
 var theConstant=3.78541;
 alert("The volume in litres is "+ volumeInLitres(gallons,theConstant)+" ltrs");
